c59dc89f32ef25b1d73356d4270e34e1ec2e5e93
Aaron Escalona <escalona.ap@gmail.com>
2023-07-12 15:53:17 -0700

    Update home page

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -32,30 +32,6 @@ export default function Index() {
       </h2>
 
       <RenderSections pageData={page} />
-
-      <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-1 sm:grid-cols-3">
-        {collections.nodes.map((collection: any) => {
-          return (
-            <Link to={`/collections/${collection.handle}`} key={collection.id}>
-              <div className="grid gap-4">
-                {collection?.image && (
-                  <Image
-                    alt={`Image of ${collection.title}`}
-                    data={collection.image}
-                    key={collection.id}
-                    sizes="(max-width: 32em) 100vw, 33vw"
-                    crop="center"
-                    width={400}
-                  />
-                )}
-                <h2 className="whitespace-pre-wrap max-w-prose font-medium text-copy">
-                  {collection.title}
-                </h2>
-              </div>
-            </Link>
-          );
-        })}
-      </div>
     </section>
   );
 }
