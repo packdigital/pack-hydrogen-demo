e880cf4e67af2a9650f35cca0a87f8edbb0afb31
Andrew Nguyen <anguy95@gmail.com>
2023-08-08 16:15:59 -0700

    feat: add in stub for footer

---
+++ app/components/PackDemoMessage.tsx
@@ -0,0 +1,17 @@
+export const PackDemoMessage = () => {
+  return (
+    <div className="bg-black p-4 rounded text-white text-sm text-center font-light">
+      <p>
+        This is a demo store brought to you by{' '}
+        <a href="https://packdigital.com" className="underline text-[#00BE8E]">
+          Pack
+        </a>
+        . <br />
+        <br />
+        <a href="github.com" className="hover:underline">
+          Explore this demo on GitHub &rarr;
+        </a>
+      </p>
+    </div>
+  );
+};
